60f7560aceed0b436917db8c17cf2af2a2e17c85
dheerajb120501 <dheeraj.bisht@smarsh.com>
2024-12-31 07:56:52 +0530

    Add packaged win forms app

---
+++ WinFormPackaged/TrayContext.cs
@@ -0,0 +1,36 @@
+﻿namespace WinFormPackaged
+{
+    public class TrayContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public TrayContext()
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Icon = SystemIcons.Information,
+                ContextMenuStrip = new ContextMenuStrip()
+                {
+                    Items = { new ToolStripMenuItem("Get Channel Identifier", null, GetChannelIdentifier) },
+                },
+                Visible = true
+            };
+        }
+
+        async void GetChannelIdentifier(object? sender, EventArgs e)
+        {
+            var wns = new WNS();
+
+            if (wns.IsSupported())
+            {
+                var channel = await wns.RequestChannelAsync();
+                trayIcon.BalloonTipText = $"Channel url copied to clipboard. Expiry in {channel.ExpirationTime}";
+                trayIcon.ShowBalloonTip(2000);
+            }
+            else
+            {
+                throw new Exception("WNS not supported");
+            }
+        }
+    }
+}
---
+++ WinFormPackaged/WNS.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Windows.PushNotifications;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace WinFormPackaged
+{
+    public class WNS
+    {
+        public WNS() { }
+
+        public Boolean IsSupported()
+        {
+            return PushNotificationManager.IsSupported();
+        }
+
+        public async Task<PushNotificationChannel> RequestChannelAsync()
+        {
+            Guid remoteId = Guid.Parse("1df54533-3101-4bd7-88f6-8de401939158");
+            var channelOperation = PushNotificationManager.Default.CreateChannelAsync(remoteId);
+
+            channelOperation.Progress = (sender, args) =>
+            {
+                if (args.status == PushNotificationChannelStatus.InProgress)
+                {
+                    Console.WriteLine("INFO: Channel request is in progress.");
+                }
+                else if (args.status == PushNotificationChannelStatus.InProgressRetry)
+                {
+                    Console.WriteLine($"ERROR: {args.extendedError} \nThe channel request is in back-off retry mode because of a retryable error! Expect delays in acquiring it. RetryCount = {args.retryCount}");
+                }
+            };
+
+            var result = await channelOperation;
+
+            if (result.Status == PushNotificationChannelStatus.CompletedSuccess)
+            {
+
+                var channelUri = result.Channel.Uri;
+
+                SetTextToClipboard(channelUri.ToString());
+                Console.WriteLine($"INFO: Channel URI: {channelUri}");
+
+                return result.Channel;
+            }
+            else if (result.Status == PushNotificationChannelStatus.CompletedFailure)
+            {
+                Console.WriteLine($"ERROR: {result.ExtendedError}\nWe hit a critical non-retryable error with channel request!");
+                throw new Exception("We hit a critical non-retryable error with channel request!");
+            }
+            else
+            {
+                Console.WriteLine($"ERROR: {result.ExtendedError}\nSome other failure occurred!");
+                throw new Exception("Some other failure occurred!");
+            }
+        }
+
+        public void SetTextToClipboard(string text) { 
+            var dataPackage = new DataPackage(); 
+            dataPackage.SetText(text); 
+            Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(dataPackage); 
+        }
+    }
+}
