468d9a479ff564f77e7b3438b9ebb1561670f417
dheerajb120501 <dheeraj.bisht@smarsh.com>
2024-12-31 09:02:42 +0530

    Add wpf packaged application

---
+++ WPFPackaged/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace WPFPackaged
+{
+    public partial class App : Application
+    {
+        private TaskbarIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose();
+            base.OnExit(e);
+        }
+    }
+
+}
---
+++ WPFPackaged/NotifyIconViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Windows.AppNotifications.Builder;
+using Microsoft.Windows.AppNotifications;
+
+namespace WPFPackaged
+{
+    public class NotifyIconViewModel
+    {
+
+        public ICommand GetChannelUriCommand
+        {
+            get { return new DelegateCommand { CommandAction = () =>  GetChannelIdentifier() }; }
+        }
+
+        async void GetChannelIdentifier()
+        {
+            var wns = new WNS();
+
+            if (wns.IsSupported())
+            {
+                var channel = await wns.RequestChannelAsync();
+                var appNotification = new AppNotificationBuilder()
+                            .AddText($"Channel Uri copied to the clipboard. Valid till {channel.ExpirationTime}")
+                            .BuildNotification();
+                AppNotificationManager.Default.Show(appNotification);
+            }
+            else
+            {
+                throw new Exception("WNS not supported");
+            }
+        }
+    }
+}
