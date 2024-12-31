70a5cc7aa447c4649d6e8f4f3dc84fec0c16122a
dheerajb120501 <dheeraj.bisht@smarsh.com>
2024-12-31 16:20:15 +0530

    Add packaged wpf application with winforms

---
+++ WPFWithWinFormsPackaged/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using NotifyIcon = System.Windows.Forms.NotifyIcon;
+
+namespace WPFWithWinFormsPackaged
+{
+    public partial class App : System.Windows.Application
+    {
+
+        private NotifyIcon trayIcon;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Icon = ProjectResources.Red,
+                ContextMenuStrip = new ContextMenuStrip()
+                {
+                    Items = { new ToolStripMenuItem("Get Channel Identifier", null, GetChannelIdentifier) },
+                },
+                Visible = true
+            };
+        
+            base.OnStartup(e);        
+        }
+
+        async void GetChannelIdentifier(object? sender, EventArgs e)
+        {
+            var wns = new WNS();
+
+            if (wns.IsSupported())
+            {
+                var channel = await wns.RequestChannelAsync();
+                trayIcon.BalloonTipText = $"Channel url copied to clipboard. Expiry in {channel.ExpirationTime}";
+                trayIcon.ShowBalloonTip(2000);
+            }
+            else
+            {
+                throw new Exception("WNS not supported");
+            }
+        }
+    }
+
+}
